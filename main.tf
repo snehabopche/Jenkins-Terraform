6afca0e440b5501dff67c15be51ebbfca04193e7
kasturenishant <50202678+kasturenishant@users.noreply.github.com>
2022-04-22 15:33:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
 provider "aws" {
     region = var.aws_region
-    
-}
-
-    
-terraform {
-  backend "s3" {
-    bucket = "tf-nk-state"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
-    dynamodb_table = "tflocktable"
-  }
 }
 
 resource "aws_instance" "ec-2" {
