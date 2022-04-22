9b9ea4c3ec716ed9eea79e6ab245675b98682255
kasturenishant <50202678+kasturenishant@users.noreply.github.com>
2022-04-22 16:14:15 -0400

    Update dynamodb_s3.tf

--- dynamodb_s3.tf
+++ dynamodb_s3.tf
@@ -1,19 +1,8 @@
-resource "aws_s3_bucket" "tf-nk-state" {
-  bucket = "${var.bucket_name}"
-  acl    = "${var.acl_value}"
-
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_dynamodb_table" "tflocktable" {
-  name             = "${var.dynaodb_name}"
-  hash_key         = "TestTableHashKey"
-  stream_enabled   = true
-
-  attribute {
-    name = "TestTableHashKey"
-    type = "S"
+terraform {
+  backend "s3" {
+    bucket = "tf-nk-state"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "tflocktable"
   }
 }
