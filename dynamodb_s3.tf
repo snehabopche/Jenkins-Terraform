53f7509af85dd5b66445fe168c5c23f769efc4fd
EC2 Default User <ec2-user@ip-172-31-11-84.us-east-2.compute.internal>
2022-04-22 21:26:00 +0000

    a

--- dynamodb_s3.tf
+++ dynamodb_s3.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "tf-nk-state"
     key    = "terraform.tfstate"
-    region = var.aws_region
+    region = "ca-central-1"
     dynamodb_table = "dynamodblocktable"
   }
 }
